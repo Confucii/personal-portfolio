4f8dae2302d008390bdeae3054f5cbfbc4fcf665
Oleksii <lesha.avdeev@gmail.com>
2023-07-05 15:51:11 +0300

    Create initial structure

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # personal-portfolio
+
 This is my portfolio website
---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import { Routes } from "react-router-dom";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes></Routes>
+    </div>
+  );
+}
+
+export default App;
